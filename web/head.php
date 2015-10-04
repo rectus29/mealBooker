62ce3a255b85bffeaae59fceb4d6ba410287ddd3
a.bernard <a.bernard@e-deal.com>
2015-10-04 18:37:31 +0200

    head improve

--- web/head.php
+++ web/head.php
@@ -4,14 +4,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-    <script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui.1.11.4.min.js" type="text/javascript"></script>
     <meta http-equiv="cache-control" content="no-cache"/>
     <meta http-equiv="pragma" content="no-cache"/>
     <meta http-equiv="expires" content="-1"/>
-    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico"/>
-    <script language="javascript" type="text/javascript" src="/js/highChartsv4/highcharts.js"></script>
-    <script language="javascript" type="text/javascript" src="/js/highChartsv4/highcharts-all.js"></script>
-    <script language="javascript" type="text/javascript" src="/js/highChartsv4/highcharts-more.js"></script>
+    <script src="<?php echo APP_PATH ?>js/jquery-1.11.3.min.js" type="text/javascript"></script>
+    <script src="<?php echo APP_PATH ?>js/jquery-ui.1.11.4.min.js" type="text/javascript"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo APP_PATH ?>img/favicon.ico"/>
+    <script language="javascript" type="text/javascript" src="<?php echo APP_PATH ?>js/highChartsv4/highcharts.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php echo APP_PATH ?>js/highChartsv4/highcharts-all.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php echo APP_PATH ?>js/highChartsv4/highcharts-more.js"></script>
     <link rel="stylesheet" href="<?php echo APP_PATH ?>web/css/app.css" media="screen" title="no title" charset="utf-8">
 </head>
