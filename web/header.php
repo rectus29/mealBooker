0e69d977471de05706bf3c33abad6a294e337ffa
Arnaud Prat <arnaud.prat@gmail.com>
2015-10-15 20:08:40 +0200

    Refactor heading & nav

--- web/header.php
+++ web/header.php
@@ -8,17 +8,9 @@
         </p>
       </div>
     </div>
-    <nav class="navbar">
-      <div class="container">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Item</a></li>
-          <li><a href="#">Item</a></li>
-          <li><a href="#">Item</a></li>
-          <li class="log-out"><a href="#">Log out</a></li>
-        </ul>
-      </div>
 
-    </nav>
+    <?php include 'nav.php' ?>
+    
   </div>
 
 </header>
---
+++ web/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+  <div class="container">
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Item</a></li>
+      <li><a href="#">Item</a></li>
+      <li><a href="#">Item</a></li>
+      <li class="log-out"><a href="#">Log out</a></li>
+    </ul>
+  </div>
+</nav>
