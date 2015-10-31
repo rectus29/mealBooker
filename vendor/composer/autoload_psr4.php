be36f34f7b1ba99ac3d2bb42cfaa8cda4c0e5c56
a.bernard <a.bernard@e-deal.com>
2015-10-31 16:04:13 +0100

    base commit

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -8,8 +8,9 @@ $baseDir = dirname($vendorDir);
 return array(
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
+    'MealBooker\\models\\dao' => array($baseDir . '/models/dao'),
     'MealBooker\\model\\' => array($baseDir . '/classes'),
     'MealBooker\\manager\\' => array($baseDir . '/manager'),
-    'MealBooker\\utils\\' => array($baseDir . '/utils'),
     'MealBooker\\' => array($baseDir . '/'),
+    'Doctrine\\Common\\Cache\\' => array($vendorDir . '/doctrine/cache/lib/Doctrine/Common/Cache'),
 );
