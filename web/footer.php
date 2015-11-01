76874e04544a6063f6e4e2d9fbb3311ad0917172
Arnaud Prat <arnaud.prat@gmail.com>
2015-11-01 21:30:49 +0100

    Layout

--- web/footer.php
+++ web/footer.php
@@ -1,7 +1,7 @@
 <footer class="container">
     <div class="assistance">
         <p>Une question ? contactez-nous par téléphone ou par mail au </p>
-        <h4>06 13 80 46 43 - aurore-traiteur@orange.fr</h4>
+        <h4>06 13 80 46 43 - <a href="mailto:aurore-traiteur@orange.fr"></a>aurore-traiteur@orange.fr</h4>
     </div>
     <div class="row">
         <div class="pull-right">
