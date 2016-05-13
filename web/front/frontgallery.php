05dca8b6386a02fa64c23718589d31ab893d5348
Arnaud Prat <arnaud.prat@gmail.com>
2016-05-13 20:10:08 +0200

    Layout

--- web/front/frontgallery.php
+++ web/front/frontgallery.php
@@ -16,21 +16,17 @@ use MealBooker\models\dao\CourseDao;
 $courseDao = new CourseDao($em);
 ?>
 
-<div class="row">
-    <div class="col-md-12">
-        <h1>Bienvenue</h1>
 
-        <p>
-            Vous êtes bien sur le système de réservation de repas Aurore Traiteur.<br/>
-            Commandez votre déjeuner pour vous le faire livrer à votre entreprise, selon un horaire pré-défini.
-        </p>
-    </div>
-</div>
 
 <div class="row courseGallery">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
+            <?php
+                $courseArray = $courseDao->getAllEnabled();
+
+                // c'est là que ça se passe
+            ?>
             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
             <li data-target="#carousel-example-generic" data-slide-to="2"></li>
@@ -41,7 +37,6 @@ $courseDao = new CourseDao($em);
             /**
              * @var $courseArray Course[]
              */
-            $courseArray = $courseDao->getAllEnabled();
             if (sizeof($courseArray) > 0) {
             for ($i = 0; sizeof($courseArray) > $i; $i++) {
                 $course = $courseArray[$i];
@@ -49,19 +44,18 @@ $courseDao = new CourseDao($em);
 
                 <div class="item <?php echo ($i < 1) ? "active" : "" ?> ">
                     <div class="row">
-                        <div class="col-md-8 img-wrapper">
-                            <img src="<?php echo APP_PATH; ?>files/course/<?php echo $course->getImg(); ?>"
-                                 alt="<?php echo $course->getName(); ?>" class="img-responsive">
-                        </div>
-                        <div class="col-md-4">
-                            <div class="">
-                                <h3><?php echo $course->getName(); ?></h3>
+                        <div class="col-md-12 img-wrapper" style="background-image:url('<?php echo APP_PATH; ?>files/course/<?php echo $course->getImg(); ?>')">
+                            <div class="col-md-4 desc">
+                                <div class="">
+                                    <h3><?php echo $course->getName(); ?></h3>
 
-                                <p><?php echo $course->getDescription(); ?></p>
-                                <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>"
-                                   class="btn btn-warning">Commander</a>
+                                    <p><?php echo $course->getDescription(); ?></p>
+                                    <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>"
+                                       class="btn btn-warning">Commander</a>
+                                </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
 
@@ -73,11 +67,11 @@ $courseDao = new CourseDao($em);
         </div>
         <!-- Controls -->
         <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
+            <i class="fa fa-chevron-left"></i>
         </a>
         <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <i class="fa fa-chevron-right"></i>
             <span class="sr-only">Next</span>
         </a>
     </div>
@@ -94,28 +88,18 @@ $courseDao = new CourseDao($em);
 }
 ?>
 
+<div class="row">
+    <div class="col-md-12">
+        <h1>Bienvenue</h1>
 
-<!--
- <div class="col-md-4">
-                <div class="card">
-                    <div class="meal-thumbnail">
-                        <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>">
-                            <img src="<?php echo APP_PATH; ?>files/course/<?php echo $course->getImg(); ?>" alt="" class="img-responsive">
-                        </a>
-                    </div>
-                    <div class="card_body">
-                        <h3>
-                            <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>">
-                            <?php echo $course->getName(); ?></h3>
-                        </a>
-                        <p>
-                            <?php echo $course->getDescription(); ?>
-                        </p>
-                        <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>" class="btn btn-warning">Commander</a>
-                    </div>
-                </div>
-            </div>
--->
+        <p>
+            Vous êtes bien sur le système de réservation de repas Aurore Traiteur.<br/>
+            Commandez votre déjeuner pour vous le faire livrer à votre entreprise, selon un horaire pré-défini.
+        </p>
+    </div>
+</div>
+
+
 
 
 
