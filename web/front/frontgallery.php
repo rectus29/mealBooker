4d7e56165c52d0b793d8c7ac536c513d9482af21
a.bernard <a.bernard@e-deal.com>
2016-05-13 20:23:22 +0200

    plop

--- web/front/frontgallery.php
+++ web/front/frontgallery.php
@@ -17,77 +17,74 @@ $courseDao = new CourseDao($em);
 ?>
 
 
-
 <div class="row courseGallery">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <?php
-                $courseArray = $courseDao->getAllEnabled();
-
-                // c'est là que ça se passe
-            ?>
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <?php
-            /**
-             * @var $courseArray Course[]
-             */
-            if (sizeof($courseArray) > 0) {
-            for ($i = 0; sizeof($courseArray) > $i; $i++) {
-                $course = $courseArray[$i];
+    <?php
+        /**
+     * @var $courseArray Course[]
+     */
+        $courseArray = $courseDao->getAllEnabled();
+        if (sizeof($courseArray) > 0) {
+        ?>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <?php
+                    for ($i = 0; sizeof($courseArray) > $i; $i++) {
+                        ?>
+                        <li data-target="#carousel-example-generic" data-slide-to="<?php echo $i ?>" class="<?php echo ($i < 1) ? "active" : "" ?>"></li>
+                        <?php
+                    }
                 ?>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+            <?php
+                for ($i = 0; sizeof($courseArray) > $i; $i++) {
+                    $course = $courseArray[$i];
+                    ?>
+                    <div class="item <?php echo ($i < 1) ? "active" : "" ?> ">
+                        <div class="row">
+                            <div class="col-md-12 img-wrapper" style="background-image:url('<?php echo APP_PATH; ?>files/course/<?php echo $course->getImg(); ?>')">
+                                <div class="col-md-4 desc">
+                                    <div class="">
+                                        <h3><?php echo $course->getName(); ?></h3>
 
-                <div class="item <?php echo ($i < 1) ? "active" : "" ?> ">
-                    <div class="row">
-                        <div class="col-md-12 img-wrapper" style="background-image:url('<?php echo APP_PATH; ?>files/course/<?php echo $course->getImg(); ?>')">
-                            <div class="col-md-4 desc">
-                                <div class="">
-                                    <h3><?php echo $course->getName(); ?></h3>
-
-                                    <p><?php echo $course->getDescription(); ?></p>
-                                    <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>"
-                                       class="btn btn-warning">Commander</a>
+                                        <p><?php echo $course->getDescription(); ?></p>
+                                        <a href="<?php echo WEB_PATH ?>?page=meal&courseID=<?php echo $course->getId(); ?>"
+                                           class="btn btn-warning">Commander</a>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
-
                     </div>
-                </div>
-
-
                 <?php
             }
             ?>
-
+            </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="sr-only">Previous</span>
+                <i class="fa fa-chevron-left"></i>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="fa fa-chevron-right"></i>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="sr-only">Previous</span>
-            <i class="fa fa-chevron-left"></i>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <i class="fa fa-chevron-right"></i>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
 
-<?php
 
-} else {
+        <?php
+
+    } else {
+        ?>
+        <div>
+            Pas de menu disponible actuellement
+        </div>
+        <?php
+    }
     ?>
-    <div>
-        Pas de menu disponible actuellement
-    </div>
-    <?php
-}
-?>
-
+</div>
 <div class="row">
     <div class="col-md-12">
         <h1>Bienvenue</h1>
