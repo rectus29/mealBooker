353ae956c837dc7ede693d55111e5b3b5c6c0b8f
a.bernard <a.bernard@e-deal.com>
2017-01-05 08:04:08 +0100

    dump update

--- mealbooker.sql
+++ mealbooker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 08 Juin 2016 à 20:43
+-- Généré le :  Jeu 05 Janvier 2017 à 08:02
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -46,31 +46,6 @@ CREATE TABLE IF NOT EXISTS `app_address` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `app_company`
---
-
-CREATE TABLE IF NOT EXISTS `app_company` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `validationCode` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
-
---
--- Contenu de la table `app_company`
---
-
-INSERT INTO `app_company` (`id`, `name`, `validationCode`, `created`, `updated`, `status`) VALUES
-(1, 'Sopra', '131', '2015-10-15 00:00:00', '2015-10-15 00:00:00', 1),
-(2, 'rrezrezrezrezrezrre', 'rezrez', '2015-11-26 21:16:41', '2015-11-26 21:16:41', 0),
-(3, 'tete', 'tete', '2016-05-09 20:16:24', '2016-05-09 20:16:24', 1);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `app_config`
 --
 
@@ -108,6 +83,7 @@ CREATE TABLE IF NOT EXISTS `app_course` (
   `status` int(11) NOT NULL,
   `img` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `nbPerDay` int(11) NOT NULL,
+  `shortDescription` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
@@ -115,11 +91,11 @@ CREATE TABLE IF NOT EXISTS `app_course` (
 -- Contenu de la table `app_course`
 --
 
-INSERT INTO `app_course` (`id`, `name`, `description`, `created`, `updated`, `status`, `img`, `nbPerDay`) VALUES
-(1, 'Menu équilibré', '<p>Menu &eacute;quilibr&eacute; 13.50 &euro; + consigne 2.50 &euro;<br /> Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et<br /> poudre de sp&eacute;culoos lpop</p>', '2015-10-15 00:00:00', '2016-05-09 19:19:53', 1, '1.jpg', 10),
-(2, 'Cassoulet', '<p>cassoulet qui fait</p>', '2015-10-13 00:00:00', '2015-11-26 22:20:55', 1, '2.jpg', 10),
-(3, 'trest', 'menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test ', '2015-11-06 00:00:11', '2015-11-26 20:52:01', 1, '132759_486112688099184_457790562_o.jpg', 99),
-(4, 'ertyuiop', '<p>ertyuiop</p>', '2015-11-06 00:27:37', '2016-05-09 20:27:25', 1, 'power-of-programmer3.png', 10);
+INSERT INTO `app_course` (`id`, `name`, `description`, `created`, `updated`, `status`, `img`, `nbPerDay`, `shortDescription`) VALUES
+(1, 'Menu équilibré', '<p>Menu &eacute;quilibr&eacute; 13.50 &euro; + consigne 2.50 &euro;<br /> Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et<br /> poudre de sp&eacute;culoos lpop</p>\r\n<p>Menu &eacute;quilibr&eacute; 13.50 &euro; + consigne 2.50 &euro;<br />Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et<br />poudre de sp&eacute;culoos lpop</p>\r\n<p>Menu &eacute;quilibr&eacute; 13.50 &euro; + consigne 2.50 &euro;<br />Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et<br />poudre de sp&eacute;culoos lpop</p>', '2015-10-15 00:00:00', '2016-12-16 17:43:27', 1, '1.jpg', 0, '<p>Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et<br />poudre de sp&eacute;culoos lpop&nbsp;Poulet au riz et carottes, sauce &agrave; l''orange Pot de cr&egrave;me au beure sal&eacute;, caramel et</p'),
+(2, 'Cassoulet', '<p>cassoulet qui fait</p>', '2015-10-13 00:00:00', '2015-11-26 22:20:55', 1, '2.jpg', 0, ''),
+(3, 'trest', 'menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test menu de test ', '2015-11-06 00:00:11', '2015-11-26 20:52:01', 1, '132759_486112688099184_457790562_o.jpg', 0, ''),
+(4, 'ertyuiop', '<p>ertyuiop</p>', '2015-11-06 00:27:37', '2016-05-09 20:27:25', 1, 'power-of-programmer3.png', 0, '');
 
 -- --------------------------------------------------------
 
@@ -177,6 +153,31 @@ INSERT INTO `app_drink` (`id`, `name`, `description`, `created`, `updated`, `sta
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `app_location`
+--
+
+CREATE TABLE IF NOT EXISTS `app_location` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lat` decimal(10,0) DEFAULT NULL,
+  `lng` decimal(10,0) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `app_location`
+--
+
+INSERT INTO `app_location` (`id`, `name`, `lat`, `lng`, `created`, `updated`, `status`) VALUES
+(1, 'A la maison', NULL, NULL, '2016-12-16 21:24:50', '2016-12-16 21:24:50', 0),
+(2, 'A boulot tres tres lojn de ma maison dans ce cas envoyer moi un mail a tete@tete.com', NULL, NULL, '2016-12-16 21:25:05', '2016-12-16 21:25:05', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `app_meal`
 --
 
@@ -195,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `app_meal` (
   KEY `IDX_9BD8AB3C8D9F6D38` (`order_id`),
   KEY `IDX_9BD8AB3C36AA4BB4` (`drink_id`),
   KEY `IDX_9BD8AB3C745B52FD` (`dessert_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `app_meal`
@@ -204,7 +205,10 @@ CREATE TABLE IF NOT EXISTS `app_meal` (
 INSERT INTO `app_meal` (`id`, `course_id`, `order_id`, `bookingId`, `created`, `updated`, `status`, `drink_id`, `dessert_id`) VALUES
 (1, 2, 1, '1462816309', '2016-05-09 19:58:37', '2016-05-09 19:58:37', 1, NULL, 2),
 (2, 1, 1, '1462816411', '2016-05-09 19:58:37', '2016-05-09 19:58:37', 1, 4, 1),
-(3, 2, 2, '1462818480', '2016-05-13 20:41:41', '2016-05-13 20:41:41', 1, 3, 1);
+(3, 2, 2, '1462818480', '2016-05-13 20:41:41', '2016-05-13 20:41:41', 1, 3, 1),
+(4, 1, 3, '1481901195', '2016-12-16 17:15:34', '2016-12-16 17:15:34', 1, 2, 1),
+(5, 1, 3, '1481903565', '2016-12-16 17:15:34', '2016-12-16 17:15:34', 1, 2, 2),
+(6, 1, 4, '1481898083', '2016-12-16 15:21:37', '2016-12-16 15:21:37', 1, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -219,18 +223,22 @@ CREATE TABLE IF NOT EXISTS `app_mealorder` (
   `updated` datetime NOT NULL,
   `status` int(11) NOT NULL,
   `timeFrame_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_1ABD6F42A76ED395` (`user_id`),
-  KEY `IDX_1ABD6F42E61AE10A` (`timeFrame_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  KEY `IDX_1ABD6F42E61AE10A` (`timeFrame_id`),
+  KEY `IDX_1ABD6F4264D218E` (`location_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `app_mealorder`
 --
 
-INSERT INTO `app_mealorder` (`id`, `user_id`, `created`, `updated`, `status`, `timeFrame_id`) VALUES
-(1, 1, '2016-05-09 19:58:37', '2016-05-09 19:58:37', 1, 1),
-(2, 1, '2016-05-13 20:41:41', '2016-05-13 20:41:41', 1, 1);
+INSERT INTO `app_mealorder` (`id`, `user_id`, `created`, `updated`, `status`, `timeFrame_id`, `location_id`) VALUES
+(1, 1, '2016-05-09 19:58:37', '2016-05-09 19:58:37', 1, 1, 1),
+(2, 1, '2016-05-13 20:41:41', '2016-05-13 20:41:41', 1, 1, 2),
+(3, 1, '2016-12-16 17:15:34', '2016-12-16 17:15:34', 1, NULL, 2),
+(4, 1, '2016-12-16 15:21:37', '2016-12-16 15:21:37', 1, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -308,7 +316,6 @@ INSERT INTO `app_timeframe` (`id`, `start`, `stop`, `created`, `updated`, `statu
 CREATE TABLE IF NOT EXISTS `app_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) DEFAULT NULL,
-  `company_id` int(11) DEFAULT NULL,
   `firstName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `lastName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -321,23 +328,23 @@ CREATE TABLE IF NOT EXISTS `app_user` (
   `updated` datetime NOT NULL,
   `status` int(11) NOT NULL,
   `restoreToken` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `company` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_88BDF3E95126AC48` (`mail`),
-  KEY `IDX_88BDF3E9D60322AC` (`role_id`),
-  KEY `IDX_88BDF3E9979B1AD6` (`company_id`)
+  KEY `IDX_88BDF3E9D60322AC` (`role_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `app_user`
 --
 
-INSERT INTO `app_user` (`id`, `role_id`, `company_id`, `firstName`, `lastName`, `mail`, `salt`, `password`, `phoneNumber`, `optIn`, `session`, `created`, `updated`, `status`, `restoreToken`) VALUES
-(1, 1, NULL, 'Admin', 'istrateur', 'contact@alexandrebernard.fr', 'GZokcpR4upD65B/avAk85XDfEM2QLg==', '$2y$10$R1pva2NwUjR1cEQ2NUIvYOnfG6sqdk7MVfJ32kvWIl1HvEKirPbbq', '0606060606', 1, 'dfjtcjl8anh3vrdmcv0kt8d436', '2015-10-15 00:00:00', '2016-06-08 20:14:23', 1, ''),
-(2, 2, 1, 'Alexandre', 'Bernard', 'rectus29@gmail.com', 'x7XsEOXrtRo1ZpydkC0Pod0Ov3XWow==', '$2y$10$eDdYc0VPWHJ0Um8xWnB5Z.SXVdgeyWEWDXg.WIA0vpOoN/LlqPs06', '+33640427440', 0, 'fgrhqgm0v1qczgvfmfepw666', '2015-11-05 21:37:57', '2015-11-26 21:18:31', 1, NULL),
-(3, 2, NULL, 'TETE', 'TETE', 'tete@teet.com', 'amULfSuL.nniBFuZR.t2aKm22DSbHA==', '$2y$10$YW1VTGZTdUwubm5pQkZ1WeJmzEsnhMCRfDBbWeG0Whn1tkgJpAdoG', '0606060606', 0, 'oyjwlhxc1anxfhuirnaeiv7i', '2016-05-06 18:10:54', '2016-05-06 18:10:54', 0, NULL),
-(4, 2, NULL, '', '', '', '7iHHdV7HJX2QWC8VlgUr8bg/C1YrZQ==', '$2y$10$N2lISGRWN0hKWDJRV0M4VeTpFJoI2odovvdnBtH.Ubvb5g/Mib2ka', '', 0, 'tvdz1qg84i3eu5w9m5yz8gvc', '2016-05-06 18:12:07', '2016-05-06 18:12:07', 0, NULL),
-(5, 2, NULL, 'tete', 'tete', 'tete@teet.fr', 'unuSS1nM924KNVbv6NqTUqVK2Tvuug==', '$2y$10$dW51U1Mxbk05MjRLTlZideJ.WI/B2MPHnYKQbYOWgfxGemzPfIWpO', '0606060606', 0, 'vmpyzyp7fz42sga14j3330un', '2016-05-06 18:15:16', '2016-05-06 18:15:16', 0, NULL),
-(6, 2, NULL, 'test', 'test', 'test@alexandrebernard.fr', 'NtdBj3BuovH4DDJ1aF8mVkq7DMCJ3w==', '$2y$10$TnRkQmozQnVvdkg0RERKMOdharPCsTec/RUNhIq8OIugs/as.lwmm', '0909090909', 0, 'nlrh0sl5h591vmbuiowsabkl', '2016-05-13 20:31:21', '2016-05-13 20:31:21', 0, NULL);
+INSERT INTO `app_user` (`id`, `role_id`, `firstName`, `lastName`, `mail`, `salt`, `password`, `phoneNumber`, `optIn`, `session`, `created`, `updated`, `status`, `restoreToken`, `company`) VALUES
+(1, 1, 'Admin', 'istrateur', 'rectus29@gmail.com', 'GZokcpR4upD65B/avAk85XDfEM2QLg==', '$2y$10$R1pva2NwUjR1cEQ2NUIvYOnfG6sqdk7MVfJ32kvWIl1HvEKirPbbq', '0606060606', 1, 'qrv9fvs48o3q09kdsb2u8iqt71', '2015-10-15 00:00:00', '2016-12-16 16:55:41', 1, '', 'La Fistiniére'),
+(2, 2, 'Alexandre', 'Bernard', 'rectus29@gmail.come', 'x7XsEOXrtRo1ZpydkC0Pod0Ov3XWow==', '$2y$10$eDdYc0VPWHJ0Um8xWnB5Z.SXVdgeyWEWDXg.WIA0vpOoN/LlqPs06', '+33640427440', 0, 'fgrhqgm0v1qczgvfmfepw666', '2015-11-05 21:37:57', '2016-12-16 16:46:54', 1, NULL, 'Ta soeur'),
+(3, 2, 'TETE', 'TETE', 'tete@teet.com', 'amULfSuL.nniBFuZR.t2aKm22DSbHA==', '$2y$10$YW1VTGZTdUwubm5pQkZ1WeJmzEsnhMCRfDBbWeG0Whn1tkgJpAdoG', '0606060606', 0, 'oyjwlhxc1anxfhuirnaeiv7i', '2016-05-06 18:10:54', '2016-05-06 18:10:54', 0, NULL, ''),
+(4, 2, '', '', '', '7iHHdV7HJX2QWC8VlgUr8bg/C1YrZQ==', '$2y$10$N2lISGRWN0hKWDJRV0M4VeTpFJoI2odovvdnBtH.Ubvb5g/Mib2ka', '', 0, 'tvdz1qg84i3eu5w9m5yz8gvc', '2016-05-06 18:12:07', '2016-05-06 18:12:07', 0, NULL, ''),
+(5, 2, 'tete', 'tete', 'tete@teet.fr', 'unuSS1nM924KNVbv6NqTUqVK2Tvuug==', '$2y$10$dW51U1Mxbk05MjRLTlZideJ.WI/B2MPHnYKQbYOWgfxGemzPfIWpO', '0606060606', 0, 'vmpyzyp7fz42sga14j3330un', '2016-05-06 18:15:16', '2016-05-06 18:15:16', 0, NULL, ''),
+(6, 2, 'test', 'test', 'test@alexandrebernard.fr', 'GZokcpR4upD65B/avAk85XDfEM2QLg==', '$2y$10$R1pva2NwUjR1cEQ2NUIvYOnfG6sqdk7MVfJ32kvWIl1HvEKirPbbq', '0909090909', 0, '', '2016-05-13 20:31:21', '2016-12-16 16:35:52', 1, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -376,6 +383,7 @@ ALTER TABLE `app_meal`
 -- Contraintes pour la table `app_mealorder`
 --
 ALTER TABLE `app_mealorder`
+  ADD CONSTRAINT `FK_1ABD6F4264D218E` FOREIGN KEY (`location_id`) REFERENCES `app_location` (`id`),
   ADD CONSTRAINT `FK_1ABD6F42A76ED395` FOREIGN KEY (`user_id`) REFERENCES `app_user` (`id`),
   ADD CONSTRAINT `FK_1ABD6F42E61AE10A` FOREIGN KEY (`timeFrame_id`) REFERENCES `app_timeframe` (`id`);
 
@@ -383,7 +391,6 @@ ALTER TABLE `app_mealorder`
 -- Contraintes pour la table `app_user`
 --
 ALTER TABLE `app_user`
-  ADD CONSTRAINT `FK_88BDF3E9979B1AD6` FOREIGN KEY (`company_id`) REFERENCES `app_company` (`id`),
   ADD CONSTRAINT `FK_88BDF3E9D60322AC` FOREIGN KEY (`role_id`) REFERENCES `app_role` (`id`);
 
 --
